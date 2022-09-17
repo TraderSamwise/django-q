60d4075be5e54aa785600696d541319173da2b6b
Sam <sam@tealstreet.io>
2022-09-16 20:15:49 -0500

    attempting to manaully patch migrations

---
+++ django_q/migrations/0003_auto_20150708_1326.py
@@ -0,0 +1,23 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('django_q', '0002_auto_20150630_1624'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='failure',
+            options={'verbose_name_plural': 'Failed tasks', 'verbose_name': 'Failed task'},
+        ),
+        migrations.AlterModelOptions(
+            name='schedule',
+            options={'verbose_name_plural': 'Scheduled tasks', 'ordering': ['next_run'], 'verbose_name': 'Scheduled task'},
+        ),
+        migrations.AlterModelOptions(
+            name='success',
+            options={'verbose_name_plural': 'Successful tasks', 'verbose_name': 'Successful task'},
+        ),
+    ]
